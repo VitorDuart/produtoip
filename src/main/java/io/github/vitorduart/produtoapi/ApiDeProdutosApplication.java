5ace44625935c9a93b8cd3d0572d47e00503b7f6
Vitor Duarte <victorduart366@gmail.com>
2024-12-11 16:59:23 -0300

    adcionado projeto spring boot

---
+++ src/main/java/io/github/vitorduart/produtoapi/ApiDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package io.github.vitorduart.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeProdutosApplication.class, args);
+	}
+
+}
